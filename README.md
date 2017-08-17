5e64e62b4735e8d71568a5e58eb8de7b72a0b6bd
OHTA Shogo <athos0220@gmail.com>
2017-08-18 08:20:34 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# abstract-machine-collection
+
+FIXME: Write a one-line description of your library/project.
+
+## Overview
+
+FIXME: Write a paragraph about the library/project and highlight its goals.
+
+## License
+
+Copyright © 2017 Shogo Ohta
+
+Distributed under the Eclipse Public License either version 1.0 or (at your option) any later version.
---
+++ project.clj
@@ -0,0 +1,46 @@
+(defproject abstract-machine-collection "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :min-lein-version "2.7.1"
+
+  :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
+                 [org.clojure/clojurescript "1.9.908"]
+                 [org.clojure/core.async  "0.3.443"]]
+
+  :plugins [[lein-figwheel "0.5.13"]
+            [lein-cljsbuild "1.1.7" :exclusions [[org.clojure/clojure]]]]
+
+  :source-paths ["src"]
+
+  :cljsbuild {:builds
+              [{:id "dev"
+                :source-paths ["src"]
+
+                :figwheel {:on-jsload "abstract-machine-collection.core/on-js-reload"}
+
+                :compiler {:main abstract-machine-collection.core
+                           :asset-path "js/compiled/out"
+                           :output-to "resources/public/js/compiled/abstract_machine_collection.js"
+                           :output-dir "resources/public/js/compiled/out"
+                           :source-map-timestamp true
+                           :preloads [devtools.preload]}}
+               {:id "min"
+                :source-paths ["src"]
+                :compiler {:output-to "resources/public/js/compiled/abstract_machine_collection.js"
+                           :main abstract-machine-collection.core
+                           :optimizations :advanced
+                           :pretty-print false}}]}
+
+  :figwheel {:css-dirs ["resources/public/css"]} 
+
+  :profiles {:dev {:dependencies [[binaryage/devtools "0.9.4"]
+                                  [figwheel-sidecar "0.5.13"]
+                                  [com.cemerick/piggieback "0.2.2"]]
+                   :source-paths ["src" "env"]
+                   :plugins [[cider/cider-nrepl "0.12.0"]]
+                   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+                   :clean-targets ^{:protect false} ["resources/public/js/compiled"
+                                                     :target-path]}})
