a1086c12e1c8316c7b2139dbb45ed0aa3ada034c
OHTA Shogo <athos0220@gmail.com>
2017-08-25 23:44:07 +0900

    Add some spec utils for dev

--- project.clj
+++ project.clj
@@ -39,7 +39,9 @@
 
   :profiles {:dev {:dependencies [[binaryage/devtools "0.9.4"]
                                   [figwheel-sidecar "0.5.13"]
-                                  [com.cemerick/piggieback "0.2.2"]]
+                                  [com.cemerick/piggieback "0.2.2"]
+                                  [orchestra "2017.07.04-1"]
+                                  [pinpointer "0.1.0-SNAPSHOT"]]
                    :source-paths ["src" "env"]
                    :plugins [[cider/cider-nrepl "0.12.0"]]
                    :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
